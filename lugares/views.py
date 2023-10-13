6a30b8b0455eae29cb8acf9cf9c07b4817e10df6
noemi777 <ada_hack505@womenintech.mx>
2023-10-12 19:42:02 -0600

    Actualización de CRUD

--- lugares/views.py
+++ lugares/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,8 +19,29 @@ class CreateLugares(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
-        data = request.data
-        serializer = LugarSerializer(data=data)
+        serializer = LugarSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'message': 'Creado'}, status=status.HTTP_201_CREATED)
+    
+class RetriveLugaresAPIView(APIView):
+    permission_classes = (AllowAny,)
+   
+    def get(self, request, pk):
+       lugares_obj = get_object_or_404(Lugar, pk=pk)
+       serializer = LugarSerializer(lugares_obj)
+       return Response(serializer.data)
+       
+    def put(self, request, pk):
+        lugares_obj = get_object_or_404(Lugar, pk=pk)
+        serializer = LugarSerializer(instance=lugares_obj, data=request.data, partial = True )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        lugares_obj = get_object_or_404(Lugar, pk=pk)
+        lugares_obj.status = False
+        lugares_obj.save()
+        return Response({'message':'Eliminado'}, status=status.HTTP_204_NO_CONTENT)
+    
